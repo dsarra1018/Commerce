0320ab4561c6b9d756f130554be34a673fd35dd5
Darren Sarra <adriansarra18@gmail.com>
2025-10-01 22:57:29 -0400

    Start listing, bid, and comment models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,21 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
+# User model
 class User(AbstractUser):
+    pass
+
+
+# Listing model
+class Listing(models.Model):
+    pass
+
+
+# Bid model
+class Bid(models.Model):
+    pass
+
+
+# Comment model
+class Comment(models.Model):
     pass
