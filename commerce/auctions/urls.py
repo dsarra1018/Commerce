2202151e36aef39076067699efeca8e89f23d1e7
Darren Sarra <adriansarra18@gmail.com>
2025-10-02 19:39:50 -0400

    Add watchlist to urlpatterns

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("categories", views.categories_view, name="category"),
+    path("watchlist", views.watchlist_view, name="watchlist"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
